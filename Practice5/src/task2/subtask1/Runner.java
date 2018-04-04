614034ded7642b0de0273745c46751518759afb9
YeshanovM <yeshanov.m@gmail.com>
2018-04-04 13:30:51 +0300

    Practice 5, task 2. Added keyboard input in subtask1

--- Practice5/src/task2/subtask1/Runner.java
+++ Practice5/src/task2/subtask1/Runner.java
@@ -1,10 +1,23 @@
 package task2.subtask1;
+import java.util.*;
 
 public class Runner {
     public static void run() {
-        System.out.println("List: " +
-                CollectionGenerator.generateList(30, -10, 10));
-        System.out.println("Set: " +
-                CollectionGenerator.generateSet(30, -10, 10));
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Input numbers count: ");
+            int length = sc.nextInt();
+            System.out.print("Input minimal number: ");
+            int min = sc.nextInt();
+            System.out.print("Input maximal number: ");
+            int max = sc.nextInt();
+            System.out.println("List: " +
+                    CollectionGenerator.generateList(length, min, max));
+            System.out.println("Set: " +
+                    CollectionGenerator.generateSet(length, min, max));
+        }
+        catch(InputMismatchException e) {
+            System.err.println("Invalid input! Program exited");
+        }
     }
 }
