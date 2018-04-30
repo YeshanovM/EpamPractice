381daa6b2b3fc9584485a3bae3f8ed55584824a2
YeshanovM <yeshanov.m@gmail.com>
2018-04-30 15:13:42 +0300

    Practice8, task4 finished

--- Practice8/src/task3/Runner.java
+++ Practice8/src/task3/Runner.java
@@ -1,6 +1,7 @@
 package task3;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.function.*;
 
 public class Runner {
     public static void run() {
@@ -16,6 +17,7 @@ public class Runner {
         System.out.println("Before: " + Arrays.toString(array));
         Arrays.sort(array, (a, b) -> Integer.compare(b, a));
         System.out.println("After: " + Arrays.toString(array));
+        System.out.println("Even: " + Arrays.toString(filter(array, x -> x % 2 == 0)));
     }
 
     public static void operateStrings() {
@@ -26,6 +28,26 @@ public class Runner {
         };
         System.out.println("Before: " + Arrays.toString(array));
         Arrays.sort(array, (a, b) -> b.compareTo(a));
-        System.out.println("Before: " + Arrays.toString(array));
+        System.out.println("After: " + Arrays.toString(array));
+        List<String> list = filter(Arrays.asList(array), s -> s.charAt(0) == 's');
+        System.out.println("Strings which starts with \"s\": " + list);
+    }
+
+    public static <T> T[] filter(T[] array, Predicate<T> predicate) {
+        ArrayList<T> result = new ArrayList<>();
+        for(T item : array) {
+            if(predicate.test(item))
+                result.add(item);
+        }
+        return (T[]) result.toArray();
+    }
+
+    public static List<String> filter(List<String> strs, Predicate<String> predicate) {
+        ArrayList<String> result = new ArrayList<>();
+        for(String item : strs) {
+            if(predicate.test(item))
+                result.add(item);
+        }
+        return result;
     }
 }
