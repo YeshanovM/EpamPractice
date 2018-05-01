d5322ac8aeaaef4521d0bb2636a0a7f82b13b573
YeshanovM <yeshanov.m@gmail.com>
2018-05-01 18:21:13 +0300

    Practice9, task2 finished

--- Practice9/src/task2/Runner.java
+++ Practice9/src/task2/Runner.java
@@ -1,19 +1,80 @@
 package task2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Runner {
-    public void run() {
+    private Integer value;
+    private boolean valueChanged;
 
+    private static final int COUNT = 1_000_000;
+
+    public void run() {
+        System.out.println("Not synchronized:");
+        startNotSynchronizedIncrementing();
+        System.out.println("\n-----------------------------\n");
+        System.out.println("Synchronized:");
+        startSynchronizedIncrementing();
     }
 
-    public List<Integer> arrayToList(int[] array) {
-        ArrayList<Integer> result = new ArrayList<>();
-        for(int element : array) {
-            result.add(element);
+    private void startNotSynchronizedIncrementing() {
+        value = 0;
+        Thread counter = new Thread(() -> {
+            while (value < COUNT)
+                value++;
+        });
+        Thread printer = new Thread(() -> {
+            while (value < COUNT)
+                System.out.println(value);
+        });
+        counter.start();
+        printer.start();
+        try {
+            printer.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        return result;
+    }
+
+    private void startSynchronizedIncrementing() {
+        value = 0;
+        valueChanged = true;
+        Thread counter = new Thread(() -> {
+            while (value < COUNT)
+                increment();
+        });
+        Thread printer = new Thread(() -> {
+            while (value < COUNT)
+                print();
+        });
+        counter.start();
+        printer.start();
+    }
+
+    private synchronized void increment() {
+        if(valueChanged) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            value++;
+            valueChanged = true;
+            notify();
+        }
+    }
+
+    private synchronized void print() {
+        if(valueChanged) {
+            System.out.println(value);
+            valueChanged = false;
+            notify();
+        }
+        else {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
